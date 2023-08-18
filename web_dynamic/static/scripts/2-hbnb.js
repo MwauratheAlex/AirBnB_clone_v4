d0b1c0d13aade10f87e8d78fdc33d93e37a3d556
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-18 11:07:00 +0300

    api status check

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -17,6 +17,16 @@ $(document).ready(function() {
 
     $("#selected-amenities").text(amenitiesStr);
 
-  })
+  });
 
+    $.get("http://0.0.0.0:5001/api/v1/status/", function(data, status) {
+      if (data.status === "OK") {
+      	$("#api_status").addClass("available");
+      } else {
+        $("#api_status").removeClass("available");
+      }
+
+    }).fail((err) => {
+      $("#api_status").removeClass("available");
+    })
 })
