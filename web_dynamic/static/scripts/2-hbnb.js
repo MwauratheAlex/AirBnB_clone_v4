cbac772fb80d4b332790d4c8afc626d33e624368
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-18 09:36:06 +0300

    selecting amenities

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  const amenities = {}
+
+  $("input").change(function() {
+    const amenityId = $(this).data("id");
+    const amenityName = $(this).data("name");
+
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else if (amenityId in amenities) {
+      delete amenities[amenityId];
+    }
+
+    let amenitiesStr = "";
+    Object.values(amenities).map((amenity) => amenitiesStr += `${amenity}, `);
+
+    $("#selected-amenities").text(amenitiesStr);
+
+  })
+
+})
